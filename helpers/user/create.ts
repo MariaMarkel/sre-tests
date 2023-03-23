a0e84be4cb7e6d058c8f770e46e88dddac68892d
MariaMarkel <volkova0512@gmail.com>
2023-03-22 20:17:35 -0700

    convert all js files to ts

--- helpers/user/create.ts
+++ helpers/user/create.ts
@@ -1,7 +1,8 @@
 // const axios = require('axios');
 import axios from 'axios';
 
-module.exports = async function create () {
+// module.exports = async function create () {
+export async function create () {
     return axios.post(`${process.env.BASE_URL}/users`, null,
     {
         headers: {
